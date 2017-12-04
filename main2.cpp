c6b3d66d14ab331823c8bc3d3e6eb97200b03539
DanilBukreev <31454061+DanilBukreev@users.noreply.github.com>
2017-12-04 17:37:31 +0300

    Create main2.cpp

---
+++ main2.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void print (int rows, int cols , int ** arr){
+if (rows == 0) {
+      cout << "Матрица пуста" << endl;
+      exit(1);
+    }
+ for (int i =0;i < rows; i++){
+       for (int j=0;j < cols; j++){
+             cout <<   arr[i][j];
+        }
+       cout << endl;
+   }
+}
+void menu() {
+  cout << " " << endl;
+  cout << "1. Вывести матрицу" << endl;
+  cout << "2. Сложить матрицу" << endl;
+  cout << "3. Умножить матрицу" << endl;
+  cout << "4. Транспонировать матрицу" << endl;
+  cout << "5. Сохранить в файл" << endl;
+  cout << "6. Загрузить из файла" << endl;
+  cout << "7. Сортировать матрицу" << endl;
+  cout << "8. Завершение работы " << endl;
+}
+
+int main(int argc, char *argv[]) {
+  int rows,cols;
+    cout << "Введите размер матрицы: " << endl;
+    cin >> rows >> cols;
+    cout << endl;
+    int **arr = new int*[rows];
+    int **arr2 = new int*[rows];
+    int **arrRezult = new int*[rows];
+    for (int i =0;i < rows; i++){
+       arr [i] = new int [cols];
+  }
+ // Заполнение матрицы
+   for (int i =0;i < rows; i++){
+       for (int j=0;j < cols; j++){
+         cin >> arr[i][j];
+       }
+
+   }
+   while(1) {
+   int c,s;
+   menu ();
+   cin >> c;
+   switch (c){
+    case 1: {
+        print (rows,cols, arr);
+        break;
+            }
+    case 2: {
+    	cout << "Введите  матрицy: " << endl;
+        cin >> rows >> cols;
+        cout << endl; 
+        for (int i =0;i < rows; i++){
+            for (int j=0;j < cols; j++){
+                cin >> arr2[i][j];
+            }
+        }
+        for (int i =0;i < rows; i++){
+            for (int j=0;j < cols; j++){
+                arrRezult[i][j] =  arr[i][j] + arr2[i][j];
+            }       
+	    }
+	    for (int i =0;i < rows; i++){
+             for (int j=0;j < cols; j++){
+                 cout <<   arrRezult[i][j];
+             }
+        }
+		break;
+	}
+    case 8:{
+           return 0;
+          }
+    }
+
+  }
+ // Освобождение памяти
+  for (int i=0;i < rows; i++){
+      delete[] arr[i];
+  }
+  delete[] arr;
+}
